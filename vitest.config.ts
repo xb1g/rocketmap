fb8dae16daecc9cc752c672d9ad8216e0645a16d
Bunyasit Fang <wachaa1319@gmail.com>
2026-02-16 08:14:04 +0000

    chore: setup vitest for TDD

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './tests/setup.ts',
+    alias: {
+      '@': resolve(__dirname, './'),
+    },
+  },
+})
